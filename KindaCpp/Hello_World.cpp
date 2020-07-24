1df8a9fb4e5e3f0601c78c35431538276d177b63
KindaOP <68750398+KindaOP@users.noreply.github.com>
2020-07-25 01:22:32 +0700

    Create Hello_World.cpp

---
+++ KindaCpp/Hello_World.cpp
@@ -0,0 +1,26 @@
+/*
+ * Yay.
+ * This is kinda like a conventional first-ever-program in any language, right?
+ * If you don't understand all of this weird syntaxes, it's fine.
+ * Just kinda look them up gradually anywhere, including in this KindaCpp directory.
+ * Anyway, here's the well-known "Hello World" code.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"hello world"<<endl;
+    return 0;
+}
+
+/*
+ * For now, all we need to take away is that executed codes are written inside that main() function.
+ * 
+ *      int main() {
+ *          //code go here
+ *          //another code goes here
+ *          //...
+ *      }
+ * 
+ */
