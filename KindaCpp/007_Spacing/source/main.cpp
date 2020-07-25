b818c4985d6817c50b3d6070f0e6bafddf689562
KindaOP <68750398+KindaOP@users.noreply.github.com>
2020-07-25 12:19:05 +0700

    Create main.cpp

---
+++ KindaCpp/007_Spacing/source/main.cpp
@@ -0,0 +1,46 @@
+/*
+ * In C++, the code is not space-sensitive.
+ * As long as there is a space, it is a space, no matter how much there is. 
+ * Believe it or not that the below code will run just fine
+ * (as long as each symbol and keyword stays intact, of course).
+ */
+
+#include                                                <iostream>
+
+                                                                        int 
+
+    main() 
+
+                                            {std::
+                cout<<"Hello World"
+                
+                                                            <<
+                                
+    std::endl;return 0
+            
+            ;}
+
+/*
+ * However, this code is almost completely unreadable.
+ * So, it is good to manage your code to be readable and organized.
+ * For example, when writing functions like main(), some people do this
+ * 
+ *      int main() {
+ *          return 0;
+ *      }
+ * 
+ * some do this
+ * 
+ *      int main()
+ *      {
+ * `        return 0;
+ *      }
+ * 
+ * sr if the code is short, some people do this
+ * 
+ *      int main() { return 0; }
+ *      
+ * It is merely a matter of styles.
+ * Personally, I barely use the first format, because I think it is hard for myself to read that.
+ * Anyway, you do you.
+ */ 
